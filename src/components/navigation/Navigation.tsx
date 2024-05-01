6fae41cc4c0c5991f21029a2fb26b48f29d17e25
Vinor <vinorval@yandex.ru>
2024-05-01 21:49:35 +0300

    перевести стили навигации в модульную систему

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -6,14 +6,14 @@ import Logo from "../../image/logo/logo.png";
 
 const Navigation = () => {
     return (
-        <section className="navigation">
-            <img className="navigation__logo" src={Logo} alt="логотип компании" />
-            <div className="navigation__block">
-                <nav className="navigation__list">
-                    <NavLink to="/company" className="navigation__link">Компания</NavLink>
-                    <NavLink to="/catalog" className="navigation__link">Очковые линзы</NavLink>
-                    <NavLink to="/breds" className="navigation__link">Бренды</NavLink>
-                    <NavLink to="/articles" className="navigation__link">Статьи</NavLink>
+        <section className={Styles.navigation}>
+            <img className={Styles.navigation__logo} src={Logo} alt="логотип компании" />
+            <div className={Styles.navigation__block}>
+                <nav className={Styles.navigation__list}>
+                    <NavLink to="/company" className={Styles.navigation__link}>Компания</NavLink>
+                    <NavLink to="/catalog" className={Styles.navigation__link}>Очковые линзы</NavLink>
+                    <NavLink to="/breds" className={Styles.navigation__link}>Бренды</NavLink>
+                    <NavLink to="/articles" className={Styles.navigation__link}>Статьи</NavLink>
                 </nav>
                 <button className={`${Styles.button} ${Styles.button_length_long}`}>Адреса салонов</button>
             </div>
